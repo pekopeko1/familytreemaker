9b5337de99760d0dd1017c53705383409a442b63
pekopeko1 <t.moi.larsson+pekoepko1@gmail.com>
2026-05-30 03:01:45 +0900

    Use polyline splines and group attribute for better vertical alignment

--- familytreemaker.py
+++ familytreemaker.py
@@ -5,29 +5,12 @@
 """familytreemaker
 
 This program creates family tree graphs from simple text files.
-
-The input file format is very simple, you describe persons of your family line
-by line, children just have to follow parents in the file. Persons can be
-repeated as long as they keep the same name or id. An example is given in the
-file LouisXIVfamily.txt.
-
-This script outputs a graph descriptor in DOT format. To make the image
-containing the graph, you will need a graph drawer such as GraphViz.
-
-For instance:
-
-$ ./familytreemaker.py -a 'Louis XIV' LouisXIVfamily.txt | \
-	dot -Tpng -o LouisXIVfamily.png
-
-will generate the tree from the infos in LouisXIVfamily.txt, starting from
-Louis XIV and saving the image in LouisXIVfamily.png.
-
 """
 
 __author__ = "Adrien Vergé"
 __copyright__ = "Copyright 2013, Adrien Vergé"
 __license__ = "GPL"
-__version__ = "1.0"
+__version__ = "1.1"
 
 import argparse
 import random
@@ -37,8 +20,8 @@ import sys
 class LayoutRuleEnforcer:
 	"""Helper to enforce and verify layout rules for DOT output.
 	Rules:
-	- Spouse connections: Horizontal (East to West, vertical center)
-	- Child connections: Vertical (South to North, horizontal center)
+	- Spouse connections: Horizontal (East to West)
+	- Child connections: Vertical (South to North)
 	- Sibling bars: Horizontal (East to West)
 	"""
 	def __init__(self):
@@ -50,16 +33,12 @@ class LayoutRuleEnforcer:
 		s, d = src, dst
 		
 		if edge_type == 'spouse':
-			# Spouse connections: horizontal from node center-east to next node center-west
-			# Use explicit port for non-invisible nodes to ensure center-point alignment
 			if ':' not in s and not s.startswith('h'): s += ':e'
 			if ':' not in d and not d.startswith('h'): d += ':w'
 		elif edge_type == 'vertical':
-			# Vertical connections: from bottom center to top center
 			if ':' not in s: s += ':s'
 			if ':' not in d: d += ':n'
 		elif edge_type == 'horizontal':
-			# Horizontal bar connections: east to west
 			if ':' not in s: s += ':e'
 			if ':' not in d: d += ':w'
 		
@@ -73,18 +52,6 @@ class LayoutRuleEnforcer:
 			(self.total_edges, self.enforced_ports)
 
 class Person:
-	"""This class represents a person.
-
-	Characteristics:
-	- name			real name of the person
-	- id			unique ID to be distinguished in a dictionnary
-	- attr			attributes (e.g. gender, birth date...)
-	- households	list of households this person belongs to
-	- follow_kids	boolean to tell the algorithm to display this person's
-					descendent or not
-
-	"""
-
 	def __init__(self, desc):
 		self.attr = {}
 		self.parents = []
@@ -113,13 +80,10 @@ class Person:
 				self.id = 'P' + hashlib.md5(self.name.encode('utf-8')).hexdigest()
 
 		self.follow_kids = True
+		self.group = None # Group for vertical alignment
 
 	def __str__(self):
 		return self.name
-
-	def dump(self):
-		return	'Person: %s (%s)\n' % (self.name, str(self.attr)) + \
-				'  %d households' % len(self.households)
 
 	def graphviz(self):
 		label = self.name
@@ -137,105 +101,62 @@ class Person:
 		opts.append('style=filled')
 		opts.append('fillcolor=' + ('F' in self.attr and 'bisque' or
 					('M' in self.attr and 'azure2' or 'white')))
+		if self.group:
+			opts.append('group=' + str(self.group))
 		return self.id + '[' + ','.join(opts) + ']'
 
 class Household:
-	"""This class represents a household, i.e. a union of two person.
-
-	Those two persons are listed in 'parents'. If they have children, they are
-	listed in 'kids'.
-
-	"""
-
 	def __init__(self):
 		self.parents = []
 		self.kids = []
 		self.id = 0
 	
-	def __str__(self):
-		return	'Family:\n' + \
-				'\tparents  = ' + ', '.join(map(str, self.parents)) + '\n' \
-				'\tchildren = ' + ', '.join(map(str, self.kids))
-
 	def isempty(self):
-		if len(self.parents) == 0 and len(self.kids) == 0:
-			return True
-		return False
+		return len(self.parents) == 0 and len(self.kids) == 0
 
 class Family:
-	"""Represents the whole family.
-
-	'everybody' contains all persons, indexed by their unique id
-	'households' is the list of all unions (with or without children)
-
-	"""
-
 	everybody = {}
 	households = []
 	layout_enforcer = LayoutRuleEnforcer()
-
-	# Use fixed size for invisible nodes to improve ortho-line stability
 	invisible = '[shape=circle,label="",height=0.01,width=0.01,fixedsize=true]';
 
 	def add_person(self, string):
-		"""Adds a person to self.everybody, or update his/her info if this
-		person already exists.
-
-		"""
 		p = Person(string)
 		key = p.id
-
 		if key in self.everybody:
 			self.everybody[key].attr.update(p.attr)
 		else:
 			self.everybody[key] = p
-
 		return self.everybody[key]
 
 	def add_household(self, h):
-		"""Adds a union (household) to self.households, and updates the
-		family members infos about this union.
-
-		"""
 		if len(h.parents) != 2:
-			print('error: number of parents != 2')
+			print('error: number of parents != 2', file=sys.stderr)
 			return
-
 		h.id = len(self.households)
 		self.households.append(h)
-
 		for p in h.parents:
 			if not h in p.households:
 				p.households.append(h)
 
 	def find_person(self, name):
-		"""Tries to find a person matching the 'name' argument.
-
-		"""
-		# First, search in ids
 		if name in self.everybody:
 			return self.everybody[name]
-		# Ancestor not found in 'id', maybe it's in the 'name' field?
 		for p in self.everybody.values():
 			if p.name == name:
 				return p
 		return None
 		
 	def populate(self, f):
-		"""Reads the input file line by line, to find persons and unions.
-
-		"""
 		h = Household()
 		while True:
 			line = f.readline()
-			if line == '': # end of file
-				if not h.isempty():
-					self.add_household(h)
+			if line == '':
+				if not h.isempty(): self.add_household(h)
 				break
 			line = line.rstrip()
 			if line == '':
-				if not h.isempty():
-					self.add_household(h)
+				if not h.isempty(): self.add_household(h)
 				h = Household()
 			elif line[0] == '#':
 				continue
@@ -249,86 +170,55 @@ class Family:
 					h.parents.append(p)
 
 	def find_first_ancestor(self):
-		"""Returns the first ancestor found.
-
-		A person is considered an ancestor if he/she has no parents.
-
-		This function is not very good, because we can have many persons with
-		no parents, it will always return the first found. A better practice
-		would be to return the one with the highest number of descendant.
-		
-		"""
 		for p in self.everybody.values():
 			if len(p.parents) == 0:
 				return p
 
 	def next_generation(self, gen):
-		"""Takes the generation N in argument, returns the generation N+1.
-
-		Generations are represented as a list of persons.
-
-		"""
 		next_gen = []
-
 		for p in gen:
-			if not p.follow_kids:
-				continue
+			if not p.follow_kids: continue
 			for h in p.households:
 				next_gen.extend(h.kids)
-				# append mari/femme
-
 		return next_gen
 
 	def get_spouse(household, person):
-		"""Returns the spouse or husband of a person in a union.
-
-		"""
 		return	household.parents[0] == person \
 				and household.parents[1] or household.parents[0]
 
 	def display_generation(self, gen):
-		"""Outputs an entire generation in DOT format.
-
-		"""
 		enforcer = Family.layout_enforcer
-
 		def print_edge(src, dst, etype, opts=None):
 			if opts is None: opts = []
 			s, d = enforcer.enforce(src, dst, etype)
 			print('\t\t%s -> %s [%s];' % (s, d, ','.join(opts)))
 
-		# Display persons
-		print('\t{ rank=same;')
+		# Assign vertical groups to middle children to force vertical lines
+		for p in gen:
+			for h in p.households:
+				if len(h.kids) > 0:
+					middle_idx = int(len(h.kids)/2)
+					h.kids[middle_idx].group = h.id
 
+		print('\t{ rank=same;')
 		prev = None
 		for p in gen:
 			l = len(p.households)
-
 			if prev:
-				if l <= 1:
-					print('\t\t%s -> %s [style=invis];' % (prev, p.id))
-				else:
-					# Ensure the invisible edge is to the first spouse found for consistent ranking
-					print('\t\t%s -> %s [style=invis];'
-						  % (prev, Family.get_spouse(p.households[0], p).id))
+				# Use weight to keep horizontal alignment tight
+				target = p.id if l == 0 else Family.get_spouse(p.households[0], p).id
+				print('\t\t%s -> %s [style=invis, weight=10];' % (prev, target))
 
 			if l == 0:
 				prev = p.id
 				continue
-			elif len(p.households) > 2:
-				raise Exception('Person "' + p.name + '" has more than 2 ' +
-								'spouses/husbands: drawing this is not ' +
-								'implemented')
-
-			# Display those on the left (if any)
+			
 			for i in range(0, int(l/2)):
 				h = p.households[i]
 				spouse = Family.get_spouse(h, p)
 				print_edge(spouse.id, 'h%d' % h.id, 'spouse', ['color="black:white:black"'])
 				print_edge('h%d' % h.id, p.id, 'spouse', ['color="black:white:black"'])
 				print('\t\th%d%s;' % (h.id, Family.invisible))
-
-			# Display those on the right (at least one)
 			for i in range(int(l/2), l):
 				h = p.households[i]
 				spouse = Family.get_spouse(h, p)
@@ -338,24 +228,17 @@ class Family:
 				prev = spouse.id
 		print('\t}')
 
-		# Display lines below households
 		print('\t{ rank=same;')
 		prev = None
 		for p in gen:
 			for h in p.households:
-				if len(h.kids) == 0:
-					continue
+				if len(h.kids) == 0: continue
 				if prev:
-					print('\t\t%s -> h%d_0 [style=invis];' % (prev, h.id))
+					print('\t\t%s -> h%d_0 [style=invis, weight=10];' % (prev, h.id))
 				l = len(h.kids)
-				if l % 2 == 0:
-					# We need to add a node to keep symmetry
-					l += 1
-				
-				# Enforce horizontal line for siblings
+				if l % 2 == 0: l += 1
 				for i in range(l - 1):
 					print_edge('h%d_%d' % (h.id, i), 'h%d_%d' % (h.id, i+1), 'horizontal')
-
 				for i in range(l):
 					print('\t\th%d_%d%s;' % (h.id, i, Family.invisible))
 					prev = 'h%d_%d' % (h.id, i)
@@ -364,73 +247,53 @@ class Family:
 		for p in gen:
 			for h in p.households:
 				if len(h.kids) > 0:
-					# Parent household junction to sibling bar
-					print_edge('h%d' % h.id, 'h%d_%d' % (h.id, int(len(h.kids)/2)), 'vertical')
+					print_edge('h%d' % h.id, 'h%d_%d' % (h.id, int(len(h.kids)/2)), 'vertical', ['group=%d' % h.id])
 					i = 0
 					for c in h.kids:
-						# sibling bar junction to child node
-						print_edge('h%d_%d' % (h.id, i), c.id, 'vertical')
+						opts = []
+						if i == len(h.kids)/2: opts.append('group=%d' % h.id)
+						print_edge('h%d_%d' % (h.id, i), c.id, 'vertical', opts)
 						i += 1
-						if i == len(h.kids)/2:
-							i += 1
+						if i == len(h.kids)/2: i += 1
 
 	def output_descending_tree(self, ancestor):
-		"""Outputs the whole descending family tree from a given ancestor,
-		in DOT format.
-
-		"""
-		# Find the first households
 		gen = [ancestor]
-
-		# Adjusting ranksep and nodesep for better stability in ortho mode
 		print('digraph {\n' + \
-		      '\tgraph [splines=ortho, nodesep=1.0, ranksep=1.0, fontname = "Meiryo UI, MS Gothic, TakaoPGothic, IPAexGothic, sans-serif"];\n' + \
-		      '\tnode [fontname = "Meiryo UI, MS Gothic, TakaoPGothic, IPAexGothic, sans-serif", shape=box, height=0.5, width=1.5];\n' + \
+		      '\tgraph [splines=polyline, nodesep=0.8, ranksep=0.6, fontname = "Meiryo UI, MS Gothic, TakaoPGothic, IPAexGothic, sans-serif"];\n' + \
+		      '\tnode [fontname = "Meiryo UI, MS Gothic, TakaoPGothic, IPAexGothic, sans-serif", shape=box, height=0.6, width=1.6, fixedsize=true];\n' + \
 		      '\tedge [fontname = "Meiryo UI, MS Gothic, TakaoPGothic, IPAexGothic, sans-serif", dir=none];\n')
-
 		for p in self.everybody.values():
 			print('\t' + p.graphviz() + ';')
 		print('')
-
 		while gen:
 			self.display_generation(gen)
 			gen = self.next_generation(gen)
-
 		print(Family.layout_enforcer.get_report())
 		print('}')
 
 def main():
-	"""Entry point of the program when called as a script.
-
-	"""
-	# Parse command line options
-	parser = argparse.ArgumentParser(description=
-			 'Generates a family tree graph from a simple text file')
-	parser.add_argument('-a', dest='ancestor',
-						help='make the family tree from an ancestor (if '+
-						'omitted, the program will try to find an ancestor)')
-	parser.add_argument('input', metavar='INPUTFILE',
-						help='the formatted text file representing the family')
+	parser = argparse.ArgumentParser(description='Generates a family tree graph from a simple text file')
+	parser.add_argument('-a', dest='ancestor', help='ancestor name or id')
+	parser.add_argument('input', metavar='INPUTFILE', help='input text file')
 	args = parser.parse_args()
-
-	# Create the family
 	family = Family()
+	try:
+		with open(args.input, 'r', encoding='utf-8') as f:
+			family.populate(f)
+	except FileNotFoundError:
+		print(f"Error: File {args.input} not found.", file=sys.stderr)
+		sys.exit(1)
 
-	# Populate the family
-	f = open(args.input, 'r', encoding='utf-8')
-	family.populate(f)
-	f.close()
-
-	# Find the ancestor from whom the tree is built
 	if args.ancestor:
 		ancestor = family.find_person(args.ancestor)
 		if not ancestor:
-			raise Exception('Cannot find person "' + args.ancestor + '"')
+			print(f'Error: Cannot find person "{args.ancestor}"', file=sys.stderr)
+			sys.exit(1)
 	else:
 		ancestor = family.find_first_ancestor()
 
-	# Output the graph descriptor, in DOT format
-	family.output_descending_tree(ancestor)
+	if ancestor:
+		family.output_descending_tree(ancestor)
 
 if __name__ == '__main__':
 	main()
